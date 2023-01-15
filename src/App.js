283afd4f83ef9d61eccb87e5ba8751b3814b51f7
MarouenC <101365894+MarouenC@users.noreply.github.com>
2023-01-15 17:59:55 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,130 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [sessiontimer, setSessiontimer] = React.useState(10);
+  const [breaktimer, setBreaktimer] = React.useState(7);
+  const [displayTime, setdisplaytime] = React.useState(sessiontimer);
+  const [timerOn, SettimerOn] = React.useState(false);
+  const [onBreak, SetonBreak] = React.useState(false);
+  const [breakaudio,setbreakaudio]= React.useState(new Audio('./huhuu.mp3'));
+const playsound=()=>{
+  breakaudio.currentTime = 0;
+  breakaudio.play();
+}
+const convertTime=(time)=>{
+  let seconds = Math.floor(time%60);
+  let minutes = Math.floor(time/60)
+  let FinalTime ="";
+  if(minutes<10){
+    minutes = "0" + minutes
+  }
+  if (minutes<1){
+    FinalTime = "00" + ":"  + seconds;
+  }
+  if(seconds<10){
+    seconds = "0" + seconds
+  }
+    FinalTime = minutes + " : " + seconds;
+  return FinalTime;
+};
+const resetTime=()=>{
+  setSessiontimer(25*60)
+  setBreaktimer(5*60)
+  setdisplaytime(25*60)
+}
+const incrementSession=()=>{
+  if (!timerOn){
+  if(sessiontimer<60*60 && sessiontimer>0){setSessiontimer(sessiontimer+60)}
+  else(setSessiontimer(25*60))
+    setdisplaytime(sessiontimer+60)
+  }
+}
+const decrementSession=()=>{
+  if (!timerOn){
+  if(sessiontimer<60*60 && sessiontimer>0){setSessiontimer(sessiontimer-60)}
+  else(setSessiontimer(25*60))
+  setdisplaytime(sessiontimer-60)
+  }
+}
+const incrementBreak=()=>{
+  if (!timerOn){
+  if(breaktimer<60*60 && breaktimer>0){setBreaktimer(breaktimer+60)}
+  else(setBreaktimer(5*60))
+}
+}
+const decremenentBreak=()=>{
+  if (!timerOn){
+  if(breaktimer<60*60 && breaktimer>0){setBreaktimer(breaktimer-60)}
+  else(setBreaktimer(5*60))
+}
+}
+const startCountdown = () => {
+ 
+  if(!timerOn){
+    const intervalId = setInterval(() => {
+      setdisplaytime((displayTime) => {
+        if(displayTime <=0 && !onBreak){ 
+          
+          SetonBreak(true);
+          playsound();
+          return breaktimer;
+        }
+        if(displayTime <=0 && onBreak){
+        
+          SetonBreak(false);
+          playsound();
+          return sessiontimer;
+        }
+        return displayTime - 1;
+      });
+    }, 1000);
+    localStorage.clear();
+    localStorage.setItem("interval-id",intervalId)
+  }
+  if(timerOn){
+    clearInterval(localStorage.getItem("interval-id"))
+  }
+  SettimerOn(!timerOn)
+};
+  return (
+  <main>
+    <div className='container' >
+      <h2> 25+5 Clock</h2>
+      <div className='row' >
+        <div className='col-md-6'>
+          <label id="break-label">Break Length</label>
+          <div className='d-flex handle-time'>
+            <button id="break-increment" className='btn btn-success ' onClick={incrementBreak}><i className="bi bi-arrow-up-circle"></i></button>
+            <input id="break-length" name="breakLen" size="2" value={convertTime(breaktimer)}/>
+            <button id="break-decrement" className='btn btn-danger ' onClick={decremenentBreak}><i className="bi bi-arrow-down-circle"></i></button>
+          </div>
+        </div>
+        <div className='col-md-6'>
+          <label id="session-label">Session length</label>
+          <div className='d-flex handle-time'>
+            <button id="session-increment" className='btn btn-success ' onClick={incrementSession}><i className="bi bi-arrow-up-circle"></i></button>
+            <input id="session-length" size="2" value={convertTime(sessiontimer)}/>
+            <button id="session-decrement" className='btn btn-danger ' onClick={decrementSession}><i className="bi bi-arrow-down-circle"></i></button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className='my-5 time-container border d-sm-block border-5 border-success rounded'>
+          <h3 id="timer-label">{!onBreak? "Session" : "Break"}</h3>
+          <div id="time-left">{convertTime(displayTime)}</div>
+        </div>
+        <div className="d-flex justify-content-evenly py-2">
+          <button id="start_stop" className='btn btn-primary ' onClick={startCountdown}> 
+          { timerOn?(<i className="bi bi-pause-fill"></i>) : (<i className="bi bi-play"></i> )}
+           <audio id="beep" preload="auto" 
+          src="https://goo.gl/65cBl1"></audio></button>
+          <button id="reset" className='btn btn-info ' onClick={resetTime}><i className="bi bi-arrow-counterclockwise"></i></button>
+        </div>
+      </div>
+    </div>
+  </main>
+  );
+}
+
+export default App;
